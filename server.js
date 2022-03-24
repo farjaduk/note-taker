3506405b3a022188163efc3ae0d7c95b8ea88b52
Manning <farjad_uk@hotmail.com>
2022-03-23 20:29:02 -0400

    post/delete/get req

--- server.js
+++ server.js
@@ -12,17 +12,58 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('Develop/public'));
 
-app.get('path', (req, res) => {});
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+);
 
-app.get('path', (req, res) => {});
-
-app.get('path', (req, res) => {});
-
-app.post('path', (req, res) => {});
-
-app.delete('path', (req, res) => {});
-
-app.listen(port, () => {
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/db/db.json"));
     
-    console.log('Server started on port');
 });
+
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    let newNote = req.body; 
+    let noteList = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+
+    
+    newNote.id = uuid();
+    
+    noteList.push(newNote); 
+
+    //updates data on database 
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(noteList)); 
+    res.json(noteList);
+
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    console.info(`${req.method} request received.`);
+
+    let noteList = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    let noteId = (req.params.id).toString();
+
+
+    noteList = noteList.filter(selected =>{
+        return selected.id != noteId;
+    })
+
+    //saves everything but selected 
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(noteList));
+
+    res.json(noteList);
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); 
+  });
